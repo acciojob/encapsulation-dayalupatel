a75d0593ac5bfdf9eba489edc98fe6f7305f0e2e
dayalu <dayalupatel98@gmail.com>
2022-10-11 16:39:17 +0530

    first try

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,13 @@
 package com.driver;
 
 public class Main {
-  
+    public static void main(String[] args) {
+        RWOnly rw = new RWOnly();
+        rw.name = "David";
+        System.out.println(rw.name);
+
+        rw.setName("John");
+        String name = rw.getName();
+        System.out.println(name);
+    }
 }
