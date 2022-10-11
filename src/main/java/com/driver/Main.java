0fcbf556030c29f45b0e0d88be1b3ce9a09e03e2
Dayalu Patel <88083299+dayalupatel@users.noreply.github.com>
2022-10-11 16:45:04 +0530

    Update Main.java

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -3,10 +3,11 @@ package com.driver;
 public class Main {
     public static void main(String[] args) {
         RWOnly rw = new RWOnly();
-        rw.name = "David";
-        System.out.println(rw.name);
+        //rw.name = "David";
+        //System.out.println(rw.name);
 
         rw.setName("John");
         String name = rw.getName();
+        System.out.println(name);
     }
 }
